656d22400f95435b1b872d5ac91eaf990ac9a148
GrashelaAyudia <grashelaayudia19@gmail.com>
2022-10-24 08:13:16 +0700

    CRUD Siswa

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloController;
+use App\Http\Controllers\SiswaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//})
+
+/*Task
+*URL :  http://localhost:8000/api/halo
+*METHOD: GET
+*Exec: function
+*Return : JSON => {"me": "Cantik"}
+*/
+
+Route::get('halo', function(){
+    $data = ["me"=> "cantik"];
+
+    return $data;
 });
+
+
+//Route::get('helocontroller', [HeloController::class, 'index']);
+//Route::post('helocontroller', [HeloController::class, 'store']);
+//Route::delete('helocontroller', [HeloController::class, 'destroy']);
+Route::resource('helocontroller', HeloController::class);
+Route::resource('siswa', SiswaController::class);
